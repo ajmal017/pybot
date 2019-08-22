7d560e8944d7689aef8673ef75c94dab4068fbd7
Paddy420 <patricknoll@gmx.de>
2019-08-22 21:22:50 +0200

    i = 0

--- vcci.py
+++ vcci.py
@@ -1,4 +1,4 @@
-import requests
+[import requests
 import json
 from indicators_full import get_atr, get_sma, get_cci
 from kursdaten import get_stock_data_wotd
@@ -38,16 +38,16 @@ def check_signal(stock):
 
     stock_data = stock_data[:min_length]
 
-    if ((stock_data[i]["close"] - stock_data[i+1]["close"]) >= 1.25*atr_1[i+1]) and ((sma[i] - sma[i+15]) > 0):
+    if ((stock_data[0]["close"] - stock_data[1]["close"]) >= 1.25*atr_1[1]) and ((sma[0] - sma[15]) > 0):
             l_high = 0
             for k in range(20):
-                vol_avg += stock_data[i+k]["volume"]
-                if stock_data[i+k]["high"] > l_high:
-                    l_high = stock_data[i+k]["high"]
+                vol_avg += stock_data[k]["volume"]
+                if stock_data[k]["high"] > l_high:
+                    l_high = stock_data[k]["high"]
 
             vol_avg = vol_avg/20
 
-            if (cci[i] * stock_data[i]["volume"]/vol_avg)  > 280 :
+            if (cci[0] * stock_data[0]["volume"]/vol_avg)  > 280 :
                     trade = {"EK" : 0, "Anzahl" : 0, "SL" : 0}
                     trade["EK"] = stock_data[0]["close"]
                     trade["SL"] = get_sl(stock)
