350c524c8c4508295e2f17d0c965ead259feaa35
Paddy420 <patricknoll@gmx.de>
2019-08-23 22:39:01 +0200

    nice triangles

--- vcci.py
+++ vcci.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from indicators_full import get_atr, get_sma, get_cci
+from indicators_full import get_atr, get_sma, get_vcci
 from kursdaten import get_stock_data_wotd
 from time import sleep
 
@@ -13,7 +13,7 @@ def check_signal(stock):
     sma_length = 50
 
     atr_1_length = 14
-    cci_length = 20
+    vcci_length = 20
 
 
     #get stock data
@@ -27,37 +27,44 @@ def check_signal(stock):
 
     atr_1 = get_atr(stock_data, atr_1_length)
 
-    cci = get_cci(stock_data, cci_length)
+    cci = get_vcci(stock_data, vcci_length)
 
     # Alle Listen so lange machen, wie die kürzeste, damit von hinten durchiteriert werden kann
-    min_length = min(len(sma),len(atr_1), len(cci))
+    min_length = min(len(sma),len(atr_1), len(vcci))
 
     sma = sma[:min_length]
     atr_1 = atr_1[:min_length]
-    cci = cci[:min_length]
+    vcci = vcci[:min_length]
 
     stock_data = stock_data[:min_length]
 
-    if ((stock_data[0]["close"] - stock_data[1]["close"]) >= atr_1[1]) and ((sma[0] - sma[15]) > 0):
+
+    if (vcci > 200 and (stock_data[0]["close"] - stock_data[1]["close"]) >= atr_1[1]):
             l_high = 0
             vol_avg = 0
-            for k in range(20):
-                vol_avg += stock_data[k]["volume"]
-                if stock_data[k]["high"] > l_high:
-                    l_high = stock_data[k]["high"]
+
+            for i in range(20):
+                vol_avg += stock_data[i]["volume"]
 
             vol_avg = vol_avg/20
 
-            if (cci[0] * stock_data[0]["volume"]/vol_avg)  > 250 :
-                stops_15 = []
-                for i in range(15):
-                    stops_15.append(stock_data[i]["high"]-5*atr_1[i])
+            if stock_data[0]["volume"] > vol_avg:
 
-                trade = {"EK" : 0, "Anzahl" : 0, "SL" : 0}
-                trade["EK"] = stock_data[0]["close"]
-                trade["SL"] = max(stops_15)
-                trade["Anzahl"] = round(300/(trade["EK"] - trade["SL"]))
 
-                output.append(stock + " - " + str(trade["Anzahl"]) + " Stück für " + str(trade["EK"]) + "SL = " + str(trade["SL"]))
+                for i in range(1, 100):
+                    if l_high < stock_data[i]["close"]:
+                        l_high = (stock_data[i]["close"]
+                if (stock_data[0]["close"] > l_high):
+
+                    stops_15 = []
+                    for i in range(15):
+                        stops_15.append(stock_data[i]["high"]-5*atr_1[i])
+
+                    trade = {"EK" : 0, "Anzahl" : 0, "SL" : 0}
+                    trade["EK"] = stock_data[0]["close"]
+                    trade["SL"] = max(stops_15)
+                    trade["Anzahl"] = round(300/(trade["EK"] - trade["SL"]))
+
+                    output.append(stock + " - " + str(trade["Anzahl"]) + " Stück für " + str(trade["EK"]) + "SL = " + str(trade["SL"]))
 
     return output
